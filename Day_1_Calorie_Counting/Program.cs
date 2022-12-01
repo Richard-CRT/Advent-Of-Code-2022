a79be328b5ab260f332ca43d7f4416fafafdf83b
Richard Carter <Richard-CRT@users.noreply.github.com>
2022-12-01 08:33:02 +0000

    Day 1 Solution

---
+++ Day_1_Calorie_Counting/Program.cs
@@ -0,0 +1,32 @@
+﻿using AdventOfCodeUtilities;
+
+List<string> inputList = AoCUtilities.GetInputLines();
+List<int> elfTotals = new List<int>();
+int runningTotal = 0;
+foreach (string str in inputList)
+{
+    if (str == "")
+    {
+        elfTotals.Add(runningTotal);
+        runningTotal = 0;
+    }
+    else
+    {
+        runningTotal += int.Parse(str);
+    }
+}
+
+void P1()
+{
+    Console.WriteLine(elfTotals.Max());
+    Console.ReadLine();
+}
+
+void P2()
+{
+    Console.WriteLine(elfTotals.OrderByDescending(x => x).ToList().GetRange(0, 3).Sum());
+    Console.ReadLine();
+}
+
+P1();
+P2();
