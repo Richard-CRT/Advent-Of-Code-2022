38eb9fd2369f4221005c1b1a86e21fad71ef1272
Richard-CRT <Richard-CRT@users.noreply.github.com>
2022-12-06 09:07:38 +0000

    Day 6 Solution

---
+++ Day_6_Tuning_Trouble/Program.cs
@@ -0,0 +1,52 @@
+﻿using AdventOfCodeUtilities;
+
+List<string> inputList = AoCUtilities.GetInputLines();
+
+void P1()
+{
+    Queue<char> buffer = new Queue<char>();
+    int count = 0;
+
+    foreach (char c in inputList[0])
+    {
+        if (buffer.Count < 4)
+            buffer.Enqueue(c);
+        else if (buffer.Distinct().Count() == 4)
+            break;
+        else
+        {
+            buffer.Dequeue();
+            buffer.Enqueue(c);
+        }
+        count++;
+    }    
+
+    Console.WriteLine(count);
+    Console.ReadLine();
+}
+
+void P2()
+{
+    Queue<char> buffer = new Queue<char>();
+    int count = 0;
+
+    foreach (char c in inputList[0])
+    {
+        if (buffer.Count < 14)
+            buffer.Enqueue(c);
+        else if (buffer.Distinct().Count() == 14)
+            break;
+        else
+        {
+            buffer.Dequeue();
+            buffer.Enqueue(c);
+        }
+        count++;
+    }
+
+    Console.WriteLine(count);
+    Console.ReadLine();
+}
+
+P1();
+P2();
